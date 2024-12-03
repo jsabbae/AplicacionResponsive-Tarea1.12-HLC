2b6f1246fc16b8416a3d6f0a293007ccdd835154
juanma785 <jsabbae2702@g.educaand.es>
2024-12-03 01:12:52 +0100

    registros introducidos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,45 @@
-
-import { Component, inject } from '@angular/core';
-import { RefresherCustomEvent } from '@ionic/angular';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+interface Cancion {
+  title: string;
+  artist: string;
+  year: number;
+  genre: string;
+}
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-  users: any;
   filtro: string = '';
+  canciones$: Observable<Cancion[]> = new Observable<Cancion[]>();
 
-  constructor(private httpClient: HttpClient) {
-    this.users = this.httpClient.get('https://randomuser.me/api/?results=20').pipe(map((res: any) => res['results']));
+  constructor(private httpClient: HttpClient) {}
+
+  ionViewWillEnter() {
+    this.loadCanciones();
+  }
+
+  loadCanciones() {
+    this.canciones$ = this.httpClient.get<Cancion[]>('assets/canciones.json'); // Ruta local al archivo JSON
+  }
+
+  applyFilter() {
+    if (this.filtro.trim()) {
+      this.canciones$ = this.canciones$.pipe(
+        map((canciones: Cancion[]) => 
+          canciones.filter(cancion => 
+            cancion.title.toLowerCase().includes(this.filtro.toLowerCase())
+          )
+        )
+      );
+    } else {
+      this.loadCanciones();
+    }
   }
 }
